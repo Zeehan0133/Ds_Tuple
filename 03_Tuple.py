a815d77fea4977ee4502c14d2d8066e0032d6bee
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:05:51 +0530

    [Zeehan] add:(unpack a tuple)

---
+++ 03_Tuple.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : unpack the tuple
+"""
+"""
+Description:
+     
+Parameter:
+      
+Return:
+       
+"""
+tuple = (4, 8, 3) 
+print(tuple)
+n1, n2, n3 = tuple
+print(n1 + n2 + n3)
