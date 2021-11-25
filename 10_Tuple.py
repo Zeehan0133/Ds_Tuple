c3d2352b674b7fa860d41fff3468697c0b075ed1
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:17:28 +0530

    [Zeehan] add:(reverse the tuple)

---
+++ 10_Tuple.py
@@ -0,0 +1,16 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : reverse a tuple 
+"""
+"""
+Description:
+      
+Parameter:
+      
+Return:
+       
+"""
+x = ("Zeehan")
+y = reversed(x)
+print(tuple(y))
