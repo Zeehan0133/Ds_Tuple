66c79af7515a6e3711898cbfd2c4039ea67d39ca
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:07:32 +0530

    [Zeehan] add:(create a colon in tuple)

---
+++ 04_Tuple.py
@@ -0,0 +1,16 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : write a program to create the colon of a tuple
+"""
+"""
+Description:
+     
+Parameter:
+      
+Return:
+       
+"""
+my_tuple = (':',)
+my_colon, = my_tuple
+print(my_colon)
