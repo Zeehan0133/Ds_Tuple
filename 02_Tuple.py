d2e848d25f17bab2c268103a24910c482ed8e3a2
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:04:08 +0530

    [Zeehan] add:(create a tuple different data type)

---
+++ 02_Tuple.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : create a tuple with different data types
+"""
+"""
+Description:
+     
+Parameter:
+      
+Return:
+       randint is a function it return random integer
+"""
+my_tuple = ('abc', 1, 4.56, ['a', 'b', 'c'], True)
+print(my_tuple)
+for i in my_tuple:
+    print(i, 'type is', type(i))
