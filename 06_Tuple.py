fb4cbcc3039029fdcdb4dda36c9ec6e30c2f7ba3
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:11:13 +0530

    [Zeehan] add:(check whether element exist or not)

---
+++ 06_Tuple.py
@@ -0,0 +1,23 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : check whether element exists or not 
+"""
+"""
+Description:
+     
+Parameter:
+      
+Return:
+       
+"""
+my_tuple = (1, 2, 3, 4, 5)
+if 2 in my_tuple:
+    print('2 is in', my_tuple)
+else:
+    print('2 is NOT in', my_tuple)
+
+if 6 in my_tuple:
+    print('6 is in', my_tuple)
+else:
+    print('6 is NOT in', my_tuple)
