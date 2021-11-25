bbbf794a0739e3267e66c2d7b4f714072511401b
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:15:55 +0530

    [Zeehan] add:(slice a tuple)

---
+++ 09_Tuple.py
@@ -0,0 +1,19 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : slice a tuple 
+"""
+"""
+Description:
+      
+Parameter:
+      
+Return:
+       
+"""
+#create a tuple
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+#used tuple[start:stop] the start index is inclusive and the stop index
+slice = tuplex[3:5]
+#is exclusive
+print(slice)
