f2fa0395830c993882af6461d720dd2575d86a0c
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:01:30 +0530

    [Zeehan] add:(create a tuple)

---
+++ 01_tuple.py
@@ -0,0 +1,18 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 19:00:30
+@Title : create a tuple
+"""
+"""
+Description:
+     
+Parameter:
+      
+Return:
+       
+"""
+tuple=(2,4,5,6,8)
+print(tuple)
+
+t2=(1,)
+print(type(t2))
