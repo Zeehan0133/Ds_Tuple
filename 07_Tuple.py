7b530f5a1dcbb9f247ad091ad0ed8eb109fda57d
Zeehan0133 <zyne0133@gmail.com>
2021-11-25 10:12:55 +0530

    [Zeehan] add:(convert a list into a tuple)

---
+++ 07_Tuple.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-24-11 13:00:30
+@Title : convert a list into a tuple
+"""
+"""
+Description:
+     
+Parameter:
+      
+Return:
+       
+"""
+listx = [5, 10, 7, 4, 15, 3]
+print(listx)
+my_tuple= tuple(listx)
+print(my_tuple)
